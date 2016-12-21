7f6a878d78c006cd4cadb999f92c4bfc296f5d36
BenDHillier <bendhillier@gmail.com>
2016-12-20 20:34:14 -0800

    friendrequests drop down works

--- scripts/getFriendRequests.js
+++ scripts/getFriendRequests.js
@@ -7,6 +7,7 @@ $('#friendRequests').click(()=>{
         Array.from($('#requestsList').children()).forEach((child)=>{
             child.remove();
         });
+        $('#requestsList').height('0px');
     }
 });
 
@@ -14,16 +15,25 @@ socket.on('getFriendRequests', (requestsList)=>{
 
     if(requestsList.length !== 0){
         console.log('recieved requests');
+        let height = requestsList.length>2 ? 150 : requestsList.length * 50;
+        $('#requestsList').height(height);
         requestsList.forEach((sender)=>{
             $('#requestsList')
-                .append($('<li>').text(sender))
-                .append($('<button>').text('add')
+                .append($('<button>').text(sender)
+                    .addClass('request')
+                    .css('background-color', 'rgb(255,230,150)')
+                    .width('80%')
                     .click(()=>{
                         socket.emit('acceptRequest');
-                    }));
+                    }))
+                .append($('<button>').text('x')
+                    .width('20%'));
         });
     } else {
-        $('#requestsList').append($('<li>').text('No Requests'));
+        $('#requestsList').height('50px')
+            .append($('<button>')
+                .text('No Requests')
+                .css('background-color', 'rgb(255,230,150)'));
     }
 
 });
@@ -36,3 +46,24 @@ socket.on('sentRequest', (success)=>{
         $('#addFriend').text('FAILED');
     }
 });
+
+function renderDropDown(list, ulId, emit, className, emptyMessage) {
+    if(list.length !== 0){
+        list.forEach((friend)=>{
+
+            $(ulId).height(height + 'px')
+                .append($('<button>')
+                    .text(friend)
+                    .addClass(className)
+                    .attr('value', friend)
+                    .css('background-color', 'rgb(255,230,150)'));
+        });
+        $('.'+className).click(function() {
+            socket.emit('openConversation', this.value);
+        });
+    } else {
+        $(ulId).height('50px')
+            .append($('<button>').text('No Friends')
+                .css('background-color', 'rgb(255,230,150)'));
+    }
+}
