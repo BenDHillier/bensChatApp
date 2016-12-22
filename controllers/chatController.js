f9ae484b9db2765d894310bc142f0d535960ece6
BenDHillier <bendhillier@gmail.com>
2016-12-21 23:34:29 -0800

    removed deprecated functions

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -5,124 +5,18 @@ let connections = [];
 module.exports = function(io, app){
     app.get('/', (req, res)=>{
         if(!req.session.user){
-
             res.redirect('/login');
-            return;
-        }
-        console.log(req.session.user);
-        model.profiles.findOne({username: req.session.user}, (err, data)=>{
-            if(!data){
-                res.render('index', Object.assign(model.createProfile(req.session.user),
-                    {friendsList: null, friendRequests: null, userNotFound:false}));
-            } else {
-                console.log('found profile for ', req.session.user);
-                res.render('index', Object.assign(data,
-                    {friendsList: null, friendRequests: null, userNotFound:false}));
-            }
-        });
-
-    });
-    /*
-    io.on('connection', function(socket){
-        let session = socket.handshake.session;
-        addConnection(session.user, socket.id);
-        socket.on('chat message', function(msg){
-            let friendID = getId(session.friend);
-            io.to(friendID).emit('chat message', session.user+": "+msg, session.user);
-            //io.to(socket.id).emit('chat message', session.user+": "+msg, session.user);
-            //update chatLog for user and friend
-            chatLogs.findOne({user: session.user, friend: session.friend}, function(err, data){
-                data.chatLog.push(session.user+ ": "+ msg);
-                chatLogs.update({user: session.user, friend: session.friend}, {chatLog: data.chatLog}, function(err){
-                });
-                chatLogs.update({user: session.friend, friend: session.user}, {chatLog: data.chatLog}, function(err){
-                });
-            });
-        });
-        socket.on('clear', function(){
-            chatLogs.findOne({user: session.user, friend: session.friend}, function(err, data){
-
-            });
-            update(session.user, session.friend, '', true, io);
-        });
-        socket.on('getFriend', function(sender, msg){
-            console.log('sender:', sender, 'msg:', msg);
-            let data = {
-                sender,
-                msg,
-                friend: session.friend
-            }
-            io.to(socket.id).emit('getFriend', data);
-        })
-    });
-    */
-
-    app.get('/chat', function(req, res){
-        if(req.session.user === null) {
-            res.redirect('/login');
-            return;
-        } else if(req.session.friend === null)
-            res.redi
-        chatLogs.findOne({user: req.session.user, friend: req.session.friend}, function(err, data) {
-            if(data){
-            res.render('index', data);
-            } else {
-                res.send('chatlog not found');
-            }
-        })
-    });
-
-    app.post('/chat', function(req, res){
-        req.session.friend = req.body.friend;
-        if(req.session.user == null) {
-            res.redirect('/login');
-            return;
-        }
-        let data = chatLogs.findOne({user: req.session.user, friend: req.session.friend}, function(err, data) {
-            if(data){
-            res.render('index', data);
-            } else {
-                res.send('chatlog not found');
-            }
-        })
-    });
-
-};
-
-function update(user, friend, msg, clear, io){
-    chatLogs.findOne({user}, function(err, data){
-        if(clear){
-            io.emit('clear')
-            chatLogs.update({user, friend}, {chatLog: []}, function(err){
-                console.log('cleared log');
-            });
         } else {
-            data.chatLog.push(msg);
-            chatLogs.update({user, friend}, {chatLog: data.chatLog}, function(err){
-                console.log('updated log');
+            model.profiles.findOne({username: req.session.user}, (err, data)=>{
+                if(!data){
+                    res.render('index', Object.assign(model.createProfile(req.session.user),
+                        {friendsList: null, friendRequests: null, userNotFound:false}));
+                } else {
+                    res.render('index', Object.assign(data,
+                        {friendsList: null, friendRequests: null, userNotFound:false}));
+                }
             });
         }
+
     });
 };
-
-function addConnection(user, id){
-    if(user){
-        let result = true;
-        connections.forEach(function(item){
-            if(item.user === user) result = false;
-        })
-        if(result)
-            connections.push({user, id});
-            console.log(connections);
-    }
-}
-
-function getId(user){
-    let id = "";
-    connections.forEach(function(item){
-        if(item.user === user) {
-            id = item.id;
-        }
-    })
-    return id;
-}
