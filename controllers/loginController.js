f8a143a81205c6951673ee2e1adf33c2cd0a7086
BenDHillier <bendhillier@gmail.com>
2016-12-29 20:26:39 -0800

    can login and signup on /login now

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -14,15 +14,30 @@ module.exports = function(app){
             req.session.friend = null;
             res.render('login', {error: ''});
             return;
+        } else if(req.body.login){
+            accounts.findOne({username: req.body.username}, function(err, data){
+                if(data && data.password === req.body.password){
+                    req.session.user = req.body.username;
+                    res.redirect('/');
+                } else {
+                    res.render('login', {error: 'invalid username or password'});
+                }
+            });
+        } else if(req.body.signup){
+            accounts.findOne({username: req.body.username}, function(err, data){
+                if(!data && req.body.username && req.body.password){
+                    Object.create(accounts({
+                        username: req.body.username,
+                        password: req.body.password,
+                    })).save();
+                    req.session.user = req.body.username;
+                    res.redirect('/');
+                } else {
+                    res.render('signup', {error: 'invalid or taken username'});
+                }
+            });
         }
-        accounts.findOne({username: req.body.user}, function(err, data){
-            if(data && data.password === req.body.password){
-                req.session.user = req.body.user;
-                res.redirect('/');
-            } else {
-                res.render('login', {error: 'invalid username or password'});
-            }
-        });
+
     });
 
 };
